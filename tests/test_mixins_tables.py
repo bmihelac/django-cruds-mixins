4c62d1bd72f2c446bf9395db64ac473424f70c1e
Bojan Mihelac <bmihelac@mihelac.org>
2018-03-13 21:24:18 +0100

    more tests

--- tests/test_mixins_tables.py
+++ tests/test_mixins_tables.py
@@ -1,6 +1,7 @@
 from django.test import RequestFactory
 from snapshottest.django import TestCase
 
+from django_tables2 import Table
 from cruds_mixins.mixins.tables import (
     TableView
 )
@@ -14,29 +15,40 @@ class TablesTest(TestCase):
 
     def setUp(self):
         self.factory = RequestFactory()
+        self.request = self.factory.get('')
         self.author = test_helper.create_author()
 
     def test_default(self):
-        request = self.factory.get('')
         view = TableView.as_view(
             model=Author,
             template_name='testapp/table.html'
         )
-        response = view(request)
+        response = view(self.request)
         self.assertEqual(response.status_code, 200)
         response.render()
         # from .browser import display; display(response.content)
         self.assertMatchSnapshot(response.content)
 
     def test_custom_table(self):
-        request = self.factory.get('')
         view = TableView.as_view(
             model=Author,
             template_name='testapp/table.html',
             table=AuthorTable
         )
-        response = view(request)
+        response = view(self.request)
         self.assertEqual(response.status_code, 200)
         response.render()
         # from .browser import display; display(response.content)
         self.assertMatchSnapshot(response.content)
+
+    def test_with_table_fields(self):
+        view = TableView.as_view(
+            model=Author,
+            template_name='testapp/table.html',
+            table_fields=('name',)
+        )
+        response = view(self.request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'name')
+        self.assertNotContains(response, 'birthday')
+
