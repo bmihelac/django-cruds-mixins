08787f4ad7d78996031f293d433273fc052afe3c
Bojan Mihelac <bmihelac@mihelac.org>
2018-05-14 11:30:26 +0200

    feat: improve pagination
    BEM classes
    Bootstrap 4 compat classes
    update test
    use custom table template in crud list

--- tests/snapshots/snap_test_templatetags_paginator.py
+++ tests/snapshots/snap_test_templatetags_paginator.py
@@ -7,148 +7,154 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestPaginatorTags::test_default 1'] = '''
+snapshots['test_paginator_tags 1'] = '''
 
 
 
+<div class="cruds-pagination">
+  
+  <p class="cruds-pagination__title">
+    100 results. Displaying: 100.
+  </p>
 
-
-
-
-    <ul class="pagination">
-        
-            <li class="prev disabled"><a>← Previous</a></li>
-        
-        
-            
-                <li class="active"><a href="?page=1">1</a></li>
-            
-        
-            
-                <li class=""><a href="?page=2">2</a></li>
-            
-        
-            
-                <li class=""><a href="?page=3">3</a></li>
-            
-        
-            
-                <li class=""><a href="?page=4">4</a></li>
-            
-        
-            
-                <li class=""><a href="?page=5">5</a></li>
-            
-        
-            
-                <li class=""><a href="?page=6">6</a></li>
-            
-        
-            
-                <li class=""><a href="?page=7">7</a></li>
-            
-        
-            
-                <li class=""><a href="?page=8">8</a></li>
-            
-        
-            
-                <li class=""><a href="?page=9">9</a></li>
-            
-        
-            
-                <li class="disabled"><a href="#">…</a></li>
-            
-        
-            
-                <li class=""><a href="?page=20">20</a></li>
-            
-        
-        
-            <li class="next"><a href="?page=2">Next →</a></li>
-        
-    </ul>
+  <ul class="cruds-pagination__pagination pagination">
+      
+          <li class="page-item prev disabled"><a class="page-link" >← Previous</a></li>
+      
+      
+          
+              <li class="page-item active"><a class="page-link" href="?page=1">1</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=2">2</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=3">3</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=4">4</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=5">5</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=6">6</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=7">7</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=8">8</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=9">9</a></li>
+          
+      
+          
+              <li class="page-item disabled"><a class="page-link"  href="#">…</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=20">20</a></li>
+          
+      
+      
+          <li class="page-item next"><a class="page-link" href="?page=2">Next →</a></li>
+      
+  </ul>
+</div>
 
 '''
 
-snapshots['TestPaginatorTags::test_default 2'] = '''
+snapshots['test_paginator_tags 2'] = '''
 
 
 
+<div class="cruds-pagination">
+  
+  <p class="cruds-pagination__title">
+    100 results. Displaying: 100.
+  </p>
 
-
-
-
-    <ul class="pagination">
-        
-            <li class="prev">
-                <a href="?page=14">← Previous</a>
-            </li>
-        
-        
-            
-                <li class=""><a href="?page=1">1</a></li>
-            
-        
-            
-                <li class="disabled"><a href="#">…</a></li>
-            
-        
-            
-                <li class=""><a href="?page=8">8</a></li>
-            
-        
-            
-                <li class=""><a href="?page=9">9</a></li>
-            
-        
-            
-                <li class=""><a href="?page=10">10</a></li>
-            
-        
-            
-                <li class=""><a href="?page=11">11</a></li>
-            
-        
-            
-                <li class=""><a href="?page=12">12</a></li>
-            
-        
-            
-                <li class=""><a href="?page=13">13</a></li>
-            
-        
-            
-                <li class=""><a href="?page=14">14</a></li>
-            
-        
-            
-                <li class="active"><a href="?page=15">15</a></li>
-            
-        
-            
-                <li class=""><a href="?page=16">16</a></li>
-            
-        
-            
-                <li class=""><a href="?page=17">17</a></li>
-            
-        
-            
-                <li class=""><a href="?page=18">18</a></li>
-            
-        
-            
-                <li class=""><a href="?page=19">19</a></li>
-            
-        
-            
-                <li class=""><a href="?page=20">20</a></li>
-            
-        
-        
-            <li class="next"><a href="?page=16">Next →</a></li>
-        
-    </ul>
+  <ul class="cruds-pagination__pagination pagination">
+      
+          <li class="page-item prev">
+              <a class="page-link" href="?page=14">← Previous</a>
+          </li>
+      
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=1">1</a></li>
+          
+      
+          
+              <li class="page-item disabled"><a class="page-link"  href="#">…</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=8">8</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=9">9</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=10">10</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=11">11</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=12">12</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=13">13</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=14">14</a></li>
+          
+      
+          
+              <li class="page-item active"><a class="page-link" href="?page=15">15</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=16">16</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=17">17</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=18">18</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=19">19</a></li>
+          
+      
+          
+              <li class="page-item "><a class="page-link" href="?page=20">20</a></li>
+          
+      
+      
+          <li class="page-item next"><a class="page-link" href="?page=16">Next →</a></li>
+      
+  </ul>
+</div>
 
 '''
--- tests/test_templatetags_paginator.py
+++ tests/test_templatetags_paginator.py
@@ -1,25 +1,20 @@
-from snapshottest.django import TestCase
-from django.template import loader, Context
+from django.template import loader
 from django.core.paginator import Paginator, Page
 
 
-class TestPaginatorTags(TestCase):
+def test_paginator_tags(snapshot):
+    objects = [
+        'object %d' % i
+        for i in range(100)
+    ]
+    paginator = Paginator(objects, 5)
+    template = loader.get_template('cruds_mixins/partials/pagination.html')
+    page = Page(objects, 1, paginator)
+    c = {'page_obj': page, 'is_paginated': True}
+    result = template.render(c)
+    snapshot.assert_match(result)
 
-    def setUp(self):
-        self.objects = [
-            'object %d' % i
-            for i in range(100)
-        ]
-        self.paginator = Paginator(self.objects, 5)
-        self.template = loader.get_template('cruds_mixins/partials/pagination.html')
-
-    def test_default(self):
-        page = Page(self.objects, 1, self.paginator)
-        c = {'page_obj': page, 'is_paginated': True}
-        result = self.template.render(c)
-        self.assertMatchSnapshot(result)
-
-        page = Page(self.objects, 15, self.paginator)
-        c = {'page_obj': page, 'is_paginated': True}
-        result = self.template.render(c)
-        self.assertMatchSnapshot(result)
+    page = Page(objects, 15, paginator)
+    c = {'page_obj': page, 'is_paginated': True}
+    result = template.render(c)
+    snapshot.assert_match(result)
